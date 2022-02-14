9d0b556d95dd985fc1864e9abbfa094200e83186
root <a07025425@itesm.mx>
2022-02-13 16:54:49 -0800

    fix it

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -6,6 +6,6 @@ def print_last_digit(number):
     print and returns the last digit number
     if given somthing thats mpt a number, this returns none
     """
-    number = (abs(number)) % 10)
+    number = (abs(number) % 10)
     print("{}".format(number), end="")
     return (int(number))
