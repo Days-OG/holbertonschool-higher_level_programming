aec0215c045fea965ac29de2b0e945a3a816166b
root <a07025425@itesm.mx>
2022-02-13 16:37:54 -0800

    fix it

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
+
 def print_last_digit(number):
     """
-print and returns the last digit number
-if given somthing thats mpt a number, this returns none
-"""
-number = (abs(number)) % 10)
-print("{}".format(number), end="")
-return (int(number))
+    print and returns the last digit number
+    if given somthing thats mpt a number, this returns none
+    """
+    number = (abs(number)) % 10)
+    print("{}".format(number), end="")
+    return (int(number))
