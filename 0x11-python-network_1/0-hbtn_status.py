857d5f8c17f4e55fde9bc7b4c596f317266c6c36
root <a07025425@itesm.mx>
2022-04-28 20:59:31 -0700

    add file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+make a request and print information about body
+'''
+
+from urllib.request import urlopen
+
+URL = 'https://intranet.hbtn.io/status'
+
+if __name__ == '__main__':
+
+    with urlopen(URL) as r:
+        body = r.read()
+        print("Body response:")
+        print("\t- type", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode())
