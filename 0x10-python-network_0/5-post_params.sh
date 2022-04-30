10c5b3d0a2c98ac5d527662f3fc2d96bc20ddbed
root <a07025425@itesm.mx>
2022-04-29 21:59:55 -0700

    add file

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a POST request to a URL and display the body
-curl -sL -d 'email=hr@holbertonschool.com'  -d 'subject=I will always be here for PDL' -- "$1"
+curl -sL -d 'email=hr@holbertonschool.com' -d 'subject=I will always be here for PLD' -- "$1"
