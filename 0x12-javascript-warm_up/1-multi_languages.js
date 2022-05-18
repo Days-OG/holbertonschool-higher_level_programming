eca28734666497b80a72c76bd284b7943b5f5632
root <a07025425@itesm.mx>
2022-05-17 17:27:08 -0700

    add file

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// script print 3 lines
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < lines.length; i += 1) {
+	console.log(lines[i]);
+}
