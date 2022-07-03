c619bb95145a00b0e323840eea98889156de3ea8
xOGx <xogx@localhost.localdomain>
2022-07-03 07:40:48 -0500

    add file multi.c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myVar = process.argv;
+if (parseInt(myVar[2])) {
+  for (let i = 0; i < myVar[2]; ++i) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
