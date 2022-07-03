795c828a6ee326336d3bb0270c01e6d6cf0147df
xOGx <xogx@localhost.localdomain>
2022-07-03 04:12:49 -0500

    apply semistd

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,5 +2,5 @@
 // script print 3 lines
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < lines.length; i += 1) {
-	console.log(lines[i]);
+  console.log(lines[i]);
 }
