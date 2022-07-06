f541b2b2ba217bf7bb1c90d701bedf71816c7d29
xOGx <xogx@localhost.localdomain>
2022-07-06 04:54:35 -0500

    add factorial function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const myVar = process.argv;
-function factorial(a){
-if (parseInt(myVar[2])) {
-  let fact = parseInt(myVar[2]);
-  for (let i = 1; i < myVar[2]; ++i) {
-    fact = fact * i;
+function factorial (a) {
+  if (parseInt(myVar[2])) {
+    let fact = parseInt(myVar[2]);
+    for (let i = 1; i < myVar[2]; ++i) {
+      fact = fact * i;
+    }
+    console.log(fact);
   }
-  console.log(fact);
- }
 }
 factorial(myVar[2]);
